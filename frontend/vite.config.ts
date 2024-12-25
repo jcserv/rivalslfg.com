ce883e677655866230f65aa3f725f0c74b48d514
jcserv <jarrod.servilla@gmail.com>
2024-12-25 11:37:55 -0500

    tweak: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
+  }
 });
