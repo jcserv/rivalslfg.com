1abadcb15508fd7b64a88c3b442e7066124307a4
jcserv <jarrod.servilla@gmail.com>
2024-12-26 23:39:48 -0500

    tweak: update favicon

--- frontend/README.md
+++ frontend/README.md
@@ -1,24 +1,16 @@
-# react-vite-shadcn-template
+# rivalslfg.com (frontend)
 
-this template can be used to quickly setup a react + typeScript + vite + shadcn site,
-with all the required configuration out of the box.
+## setup
 
-## features
+### prerequisites
+- node
+- pnpm
 
-- ✨ components & styling with tailwindcss & shadcn
-- 📦 bundling via vite
-- 🔀 routing w/ tanStack router
-- 🧪 unit/component tests setup (see Header.cy.tsx & utils.test.ts)
-- 🌑 dark mode
-- ✅ continuous integration/deployment with github workflows & vercel
-
-## dependencies
-
-- [vite](https://vite.dev/)
-- [tailwindcss](https://tailwindcss.com/)
-- [shadcn](https://ui.shadcn.com/docs)
-- [tanstack router](https://tanstack.com/router/latest/docs/framework/react/overview)
+### installation
+1. Clone the repo
+2. Run `pnpm install`
+3. Run `pnpm run dev`
 
 ## references
 
-- [shadcn-vite template](https://github.com/shadcn/vite-template)
+- [favicon.io/](https://favicon.io/)
--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -1,15 +1,3 @@
-import dog from "@/assets/dog.jpg";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/")({
@@ -19,36 +7,10 @@ export const Route = createLazyFileRoute("/")({
 function Index() {
   return (
     <section className="p-2 text-center">
-      <h3>Welcome to my blog!</h3>
+      
       <div className="h-screen w-screen flex items-center justify-center">
-        <Content />
+      <h3>Welcome to my blog!</h3>
       </div>
     </section>
-  );
-}
-
-function Content() {
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <img
-          src={dog}
-          alt="A dog typing on a computer with the caption: I have no idea what I am doing"
-        />
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will drop all tables in
-            production and it&apos;s the first day of your internship!
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
   );
 }
