df96b50bb8133b39c09a44d299b33c4bd94c2479
jcserv <jarrod.servilla@gmail.com>
2025-01-02 09:21:35 -0500

    tidy: move sentry init to logger

--- backend/cmd/rivalslfg/main.go
+++ backend/cmd/rivalslfg/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"context"
-	"time"
 
-	"github.com/getsentry/sentry-go"
 	"github.com/jcserv/rivalslfg/internal"
-	"github.com/jcserv/rivalslfg/internal/utils/env"
 	"github.com/jcserv/rivalslfg/internal/utils/log"
 	"go.uber.org/zap"
 )
@@ -14,16 +11,6 @@ import (
 func main() {
 	logger := log.GetLogger(context.Background())
 	defer logger.Sync()
-
-	if err := sentry.Init(sentry.ClientOptions{
-		Dsn:              env.GetString("SENTRY_DSN", ""),
-		AttachStacktrace: true,
-	}); err != nil {
-		logger.Fatal("could not initialize sentry", zap.Error(err))
-	}
-
-	defer sentry.Flush(2 * time.Second)
-	sentry.CaptureMessage("It works!")
 
 	service, err := internal.NewService()
 	if err != nil {
--- backend/internal/utils/log/log.go
+++ backend/internal/utils/log/log.go
@@ -2,7 +2,9 @@ package log
 
 import (
 	"context"
+	"time"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/jcserv/rivalslfg/internal/utils/env"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -12,7 +14,36 @@ var (
 	logger *zap.Logger
 )
 
+func initSentry() error {
+	return sentry.Init(sentry.ClientOptions{
+		Dsn:              env.GetString("SENTRY_DSN", ""),
+		AttachStacktrace: true,
+		EnableTracing:    true,
+		TracesSampleRate: 1.0,
+		Environment:      env.GetString("ENVIRONMENT", "dev"),
+	})
+}
+
+func sentryHook(entry zapcore.Entry) error {
+	if entry.Level >= zapcore.ErrorLevel {
+		hub := sentry.CurrentHub()
+		event := sentry.NewEvent()
+		event.Level = sentry.LevelError
+		event.Message = entry.Message
+		event.Timestamp = entry.Time
+		hub.CaptureEvent(event)
+	}
+	return nil
+}
+
 func Init(isProd bool) *zap.Logger {
+	if isProd {
+		if err := initSentry(); err != nil {
+			panic(err)
+		}
+		defer sentry.Flush(2 * time.Second)
+	}
+
 	config := zap.NewProductionConfig()
 	if !isProd {
 		config = zap.NewDevelopmentConfig()
@@ -24,7 +55,16 @@ func Init(isProd bool) *zap.Logger {
 	}
 
 	l, _ := config.Build()
-	logger = l.WithOptions(zap.AddCallerSkip(1))
+
+	options := []zap.Option{zap.AddCallerSkip(1)}
+	if isProd {
+		sentryCore := zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+			return zapcore.RegisterHooks(core, sentryHook)
+		})
+		options = append(options, sentryCore)
+	}
+
+	logger = l.WithOptions(options...)
 	return logger
 }
 
