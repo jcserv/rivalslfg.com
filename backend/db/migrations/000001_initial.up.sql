b25e969f8f7a6168b21ea9767d892c1ed270a70a
jcserv <jarrod.servilla@gmail.com>
2025-01-05 10:05:56 -0500

    chore: bring back player table

--- backend/db/migrations/000001_initial.up.sql
+++ backend/db/migrations/000001_initial.up.sql
@@ -1,57 +1,3 @@
--- Types
-
-CREATE TYPE RankName AS ENUM (
-    'Bronze III',
-    'Bronze II',
-    'Bronze I',
-    'Silver III',
-    'Silver II',
-    'Silver I',
-    'Gold III',
-    'Gold II',
-    'Gold I',
-    'Platinum III',
-    'Platinum II',
-    'Platinum I',
-    'Diamond III',
-    'Diamond II',
-    'Diamond I',
-    'Grandmaster III',
-    'Grandmaster II',
-    'Grandmaster I',
-    'Eternity',
-    'One Above All'
-);
-
-CREATE TYPE RankID AS ENUM (
-    'b3', -- 0
-    'b2', -- 1
-    'b1', -- 2
-    's3', -- 10
-    's2', -- 11
-    's1', -- 12
-    'g3', -- 20
-    'g2', -- 21
-    'g1', -- 22
-    'p3', -- 30
-    'p2', -- 31
-    'p1', -- 32
-    'd3', -- 40
-    'd2', -- 41
-    'd1', -- 42
-    'gm3', -- 50
-    'gm2', -- 51
-    'gm1', -- 52
-    'e', -- 60
-    'oa' -- 70
-);
-
-CREATE TYPE Rank AS (
-    id RankID,
-    name RankName,
-    val integer
-);
-
 -- Functions
 
 CREATE OR REPLACE FUNCTION generate_group_id() 
@@ -113,7 +59,7 @@ CREATE TABLE Community (
 CREATE TABLE Groups (
     id CHAR(4) PRIMARY KEY DEFAULT generate_group_id(),
     community_id INTEGER NOT NULL REFERENCES Community(id) DEFAULT 1,
-    owner VARCHAR(14) NOT NULL, -- this is the player's name
+    owner VARCHAR(14), -- this is the player's name
     region CHAR(2) NOT NULL,
     gamemode TEXT NOT NULL,
     players JSONB NOT NULL DEFAULT '[]'::JSONB,
@@ -134,4 +80,27 @@ CREATE TABLE Groups (
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     last_active_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE Players (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(14) NOT NULL,
+    platform CHAR(2) NOT NULL,
+    roles TEXT[] NOT NULL,
+    rank INTEGER NOT NULL,
+    characters TEXT[] NOT NULL,
+    voice_chat BOOLEAN NOT NULL,
+    mic BOOLEAN NOT NULL,
+    
+    -- role_queue
+    vanguards INTEGER,
+    duelists INTEGER,
+    strategists INTEGER
+);
+
+CREATE TABLE GroupMembers (
+    group_id CHAR(4) NOT NULL REFERENCES Groups(id),
+    player_id INTEGER NOT NULL REFERENCES Players(id),
+    leader BOOLEAN NOT NULL,
+    PRIMARY KEY (group_id, player_id)
 );
